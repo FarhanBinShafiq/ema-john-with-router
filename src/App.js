651660ae96e0e17a9b395b3f93c005d5fb0cb17f
FarhanBinShafiq <farhan.shafiq@northsouth.edu>
2021-12-22 18:56:44 +0600

    product and overview file update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { BrowserRouter , Routes,Route} from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import OrderReview from './components/OrderReview/OrderReview';
+import NotFound from './components/NotFound/NotFound';
+import Inventory from './components/Inventory/Inventory';
 
 function App() {
   return (
     <div>
-      <Header></Header>
-      <Shop></Shop>
+     <BrowserRouter>
+        <Header></Header>
+          <Routes>
+           <Route path="shop" element={<Shop></Shop>}></Route>
+           <Route path="orderreview" element={<OrderReview></OrderReview>}></Route>
+           <Route path="*" element={<NotFound></NotFound>}></Route>
+           <Route path="inventory" element={<Inventory></Inventory>}></Route>
+          </Routes>
+     </BrowserRouter>
     </div>
   );
 }
--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
+import useProducts from '../../hooks/useProducts';
+
+
 
 const OrderReview = () => {
+
+  const[products]=useProducts()
+  const [cart]=useCart(products)
+
     return (
         <div>
             <h2>This is Order Review</h2>
+            <p>{products.length}</p>
+            <h3>{cart.length}</h3>
         </div>
     );
 };
